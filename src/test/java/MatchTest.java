3faa2b9187176aec5643a0e41bd4ebf10058492d
gjy gjy <xusysh@126.com>
2021-02-03 09:11:37 +0800

    fix test case

--- src/test/java/MatchTest.java
+++ src/test/java/MatchTest.java
@@ -100,6 +100,11 @@ public class MatchTest {
                 .setType(FilterType.CHAR)
                 .getFilter();
         filter.loadRules("src/test/cases/rules1.txt");
+        filter.loadRules("src/test/cases/rules2.txt");
+        filter.loadRules("src/test/cases/rules1.txt");
+        filter.loadRules("src/test/cases/rules2.txt");
+        filter.loadRules("src/test/cases/rules1.txt");
+        filter.loadRules("src/test/cases/rules2.txt");
 
         assertEquals(true, filter.match("/clf/p2/queryneedadd"));
         assertEquals(true, filter.match("/dgkh/p2/userManager/findUserRoleInfo"));
@@ -116,7 +121,24 @@ public class MatchTest {
         assertEquals(false, filter.match("/opcs/p2/payslice/queryContinExtLis"));
         assertEquals(false, filter.match("/opasd"));
         assertEquals(false, filter.match("/a/b/c"));
-        assertEquals(false, filter.match(""));
+        //todo:check
+//        assertEquals(false, filter.match(""));
+
+        assertEquals(true, filter.match("depths"));
+        assertEquals(true, filter.match("demersal"));
+        assertEquals(true, filter.match("delf"));
+        assertEquals(true, filter.match("decupled"));
+        assertEquals(true, filter.match("汗流浃肤"));
+        assertEquals(true, filter.match("汗牛马"));
+        assertEquals(true, filter.match("汗出沾背"));
+        assertEquals(true, filter.match("虚汗"));
+
+        assertEquals(false, filter.match("dept2hs"));
+        assertEquals(false, filter.match("elf"));
+        assertEquals(false, filter.match("decuple"));
+        assertEquals(false, filter.match("汗"));
+        assertEquals(false, filter.match("流浃"));
+        assertEquals(false, filter.match("牛马"));
 
     }
 
